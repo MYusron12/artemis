e5c6f8a4a011b51ebce91bb6e17c3966aabf51d3
devyusron <muhammad.yusron@jadinpratama.com>
2026-04-09 17:09:00 +0700

    add index html

--- public/index.php
+++ public/index.php
@@ -2,4 +2,13 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-echo "Artemis berjalan bousx!";
+$uri = $_SERVER['REQUEST_URI'];
+
+// Kalau akses root, tampilkan landing page
+if ($uri === '/' || $uri === '') {
+    readfile(__DIR__ . '/index.html');
+    exit;
+}
+
+// Selain itu, jalankan framework seperti biasa
+echo "Artemis berjalan!";
