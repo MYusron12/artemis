c7df014ea21343b82dbb2f8b5e710b39232e049b
devyusron <muhammad.yusron@jadinpratama.com>
2026-04-11 11:41:52 +0700

    add landing page

--- public/index.php
+++ public/index.php
@@ -6,13 +6,11 @@ use Artemis\Router;
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-// Kalau akses root, tampilkan landing page
 if ($uri === '/' || $uri === '') {
     readfile(__DIR__ . '/index.html');
     exit;
 }
 
-// Selain itu, jalankan framework
 $router = new Router();
 
 require __DIR__ . '/../routes/api.php';
